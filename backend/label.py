22cf882b6116a6b61c7f0e22a124d006e3e0c889
perceptronq <hungrydude283@gmail.com>
2025-01-19 16:12:08 +0530

    integrate vit

--- backend/label.py
+++ backend/label.py
@@ -1,4 +1,5 @@
 from vlm import generate_description
+from vit import VisionTransformer
 import en_core_web_sm
 
 nlp = en_core_web_sm.load()
@@ -11,6 +12,7 @@ def detect_labels_uri(uri):
     client = vision.ImageAnnotatorClient()
     image = vision.Image()
     image.source.image_uri = uri
+    vit = VisionTransformer()
 
     response = client.label_detection(image=image)
     labels = response.label_annotations
@@ -19,7 +21,7 @@ def detect_labels_uri(uri):
     final_labels = []
     for label in labels:
         final_labels.append(label.description)
-
+    
     # Generate description and extract important words
     description = generate_description(uri)
     doc = nlp(description)
@@ -29,7 +31,10 @@ def detect_labels_uri(uri):
         if ent.label_ in ("GPE", "ORG", "PERSON", "LOC", "PRODUCT", "EVENT", "NORP", "FAC", "LAW", "WORK_OF_ART", "LANGUAGE")
     ]
     final_labels.extend(important_words)
-
+    
+    more_labels = vit.imagetag(uri)
+    for l in more_labels:
+        final_labels.append(l)
     final_labels = list(set(final_labels))
 
     print(final_labels)
